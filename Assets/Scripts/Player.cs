eaf31b85ef253de27d01c84dd25e3cc1aa20680b
Murilo Estacio <muriloestacio1@gmail.com>
2025-06-23 20:53:29 -0300

    Substituição da capsula por PlayerVisual e adição de rotação na movimentação

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,6 +32,7 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        //Debug.Log(inputVector);
+        float rotationSpeed = 15f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 }
